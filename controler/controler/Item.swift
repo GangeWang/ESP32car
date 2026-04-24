ab9a5cba2346ac0f5313be7c99a0f19d5893333a
gange <william20040911@gmail.com>
2026-04-24 15:09:17 +0800

    修正一些錯誤

---
+++ controler/controler/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  controler
+//
+//  Created by Gange on 2026/4/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ controler/controlerTests/controlerTests.swift
@@ -0,0 +1,19 @@
+//
+//  controlerTests.swift
+//  controlerTests
+//
+//  Created by Gange on 2026/4/24.
+//
+
+import Testing
+@testable import controler
+
+struct controlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
